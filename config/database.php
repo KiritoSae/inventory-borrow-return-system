53e67b7ca95322f3dcf8aa49fef245e997b92134
KiritoSae <perezerdyn@gmail.com>
2026-08-25 11:10:08 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,1 +1,20 @@
+<?php
 
+$host = "localhost";
+$dbname = "inventory_system";
+$username = "root";
+$password = "";
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+        $username,
+        $password
+    );
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+    die("Database connection failed.");
+}
+?>
