eadc53301669bf43c429f8766bb40aef61d2f016
KiritoSae <perezerdyn@gmail.com>
2026-08-25 11:14:25 +0800

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,18 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Inventory Management System</title>
+
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+
+<body>
